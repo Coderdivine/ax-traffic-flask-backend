1fba7511357ba3b491584ef9726e512cd31305a5
Coderdivine <chimdi4332@gmail.com>
2022-05-14 13:28:33 +0100

    kskwbknvk

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ import pandas as pd
 app = Flask(__name__)
 model = NeuralNetwork(lr=0.01,iter=200)
 #x  = np.random.randn(4,10)
-y = np.random.randn(1,)
+#y = np.random.randn(1,)
 @app.route("/train", methods=['GET'])
 def trainData():
-    #data = requests.get("https://ax-traffic.herokuapp.com/get-data/passed")
-    data = requests.get("https://ax-short.herokuapp.com/link234tools-get")
+    data = requests.get("https://ax-traffic.herokuapp.com/get-data/passed")
+    #data = requests.get("https://ax-short.herokuapp.com/link234tools-get")
     json_data = json.loads(data.content)
     result = list(json_data)
     df = pd.DataFrame(json_data)
-    df = df.drop(["longURL","date","shortURL"], axis=1)
-    x =np.array(df.iloc[:,0:3].astype(int)).T
-    y = np.array(df.iloc[:,3:].astype(int)).T
+    #df = df.drop(["longURL","date","shortURL"], axis=1)
+    x =np.array(df.iloc[:,0:5].astype(int)).T
+    y = np.array(df.iloc[:,5:].astype(int)).T
     model.fit(x,y)
     pickle.dump(model,open("models.pkl", "wb"))
     return jsonify({"data":{
@@ -27,29 +27,21 @@ def trainData():
     }})
 @app.route("/predict", methods=['GET','POST'])
 def predict():
-    """
     time = request.form["time"]
     period =  request.form["period"]
     long = request.form["long"]
     lat = request.form["lat"]
-    data = np.array([[time,period,long,lat]])
+    date = request.form["date"]
+    data = np.array([{period,time,long,lat,date}])
     model = pickle.load(open("model.pkl", "rb"))
     pred = model.predict(data)
-    listed = model.NewRoadSpeed(pred[0])
-    """
-    xtest = np.random.choice([1.45,3.45,6.97,2.83],size=(1,3)).T
-    model = pickle.load(open("models.pkl", "rb"))
-    a2 = model.predict(xtest) 
-    cost = model.cost(y,a2)  
-    pred = model.NewRoadSpeed(float(a2[0][0]))
-    return jsonify({"data":[{
-        "data":xtest[0][0],
-        "pred":a2[0][0]
-    },
-    {
-        "speed":pred,
-        "cost":cost
-    }]})
+    preds = model.NewRoadSpeed(float(pred[0][0]))
+    return jsonify({
+        "data":{
+            "pred":pred[0][0],
+            "speedlimits":preds
+        }
+    })
     
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -1,6 +1,9 @@
+import pickle
 import numpy as np
+import pickle
+"""
 class NeuralNetwork:
-    def __init__(self,lr=0.001,iter=100,hidden=6):
+    def __init__(self,lr=0.01,iter=10100,hidden=12):
         self.iter = iter
         self.lr =lr
         self.hidden = hidden
@@ -10,7 +13,6 @@ class NeuralNetwork:
         self.b2 =None
         
     def fit(self,x,y):
-        n_samples,n_features = x.shape
         m,n =y.shape
         self.w1 =np.random.randn(self.hidden,len(x))*0.01
         self.b1 =np.zeros((self.hidden,1))
@@ -78,18 +80,93 @@ class NeuralNetwork:
         }
         return caching
 print("working")
-#x  = np.random.randn(6,4)
-#y = np.random.randn(6,1)
-#model = NeuralNetwork()
-#x = x.T
-#y = y.T
-#xtest = np.random.randn(3,4).T
-#model.fit(x,y)
-#pickle.dump(model,open("model.pkl", "wb"))
-# #data = np.array([[1.3,2.4,1.6,1.12]]).T
-#model = pickle.load(open("model.pkl", "rb"))
-#a2 = model.predict(xtest)
-#print(a2)
-#a2 = model.NewRoadSpeed(float(a2[0]))
 
+"""
+
+
+
+
+class NeuralNetwork:
+    def __init__(self,lr=0.001,iter=210050,hidden=68):
+        self.lr = lr
+        self.iter = iter
+        self.hidden = hidden
+        self.w1 = None 
+        self.b1 = None
+        self.w2 = None 
+        self.b2 = None
+    def fit(self,x,y):
+        n_samples,n_features = x.shape
+        m,n=y.shape
+        self.w1 = np.random.randn(self.hidden,len(x))*0.01
+        self.b1 = np.zeros((self.hidden,1))    
+        self.w2 = np.random.randn(1,self.hidden)*0.01
+        self.b2 = np.zeros((1,1))  
+        m = y.size
+        for _ in range(self.iter):
+            z1 = np.dot(self.w1,x)+self.b1 
+            a1 = self.hyperbolic(z1)
+            z2 = np.dot(self.w2,a1)+self.b2
+            a2 = self.sigmoid(z2)
+            dz2 = a2 - y
+            dw2 = (1/m)*np.dot(dz2,a1.T)
+            db2 = (1/m)*np.sum(dz2,axis=1,keepdims=True)
+            dz1 = np.dot(self.w2.T,dz2)*(1-np.power(z1,2))
+            dw1 = (1/m)*np.dot(dz1,x.T)
+            db1 = (1/m)*np.sum(dz1,axis=1,keepdims=True)
+            
+            self.w1 = self.w1 - (self.lr*dw1)
+            self.w2 = self.w2 - (self.lr*dw2)      
+            self.b1 = self.b1 - (self.lr*db1)
+            self.b2 = self.b2 - (self.lr*db2)
+    def prediction(self,x):
+        z1 = np.dot(self.w1,x)+self.b1
+        a1 = self.hyperbolic(z1)
+        z2 = np.dot(self.w2,a1)+self.b2
+        a2= self.sigmoid(z2)
+         #a2 = [1 if i>0.5 else 0 for i in a2[0]]
+        return a2
+    def sigmoid(self,z):
+        return 1/(1+np.exp(-z))
+    def hyperbolic(self, z):
+        return np.tanh(z)        
+        
+"""
+X = np.array([
+    [0.11,0.9,0.8,0.98],
+    [0.12,0.89,0.73,0.99],
+    [0.34,0.41,0.71,0.81],
+    [0.13,0.14,0.16,0.71],
+    [0.54,0.55,0.41,0.39],
+    [0.11,0.41,0.43,0.23]
+])
+Y = np.array([
+    [0],
+    [1],
+    [0],
+    [0],
+    [1],
+    [0]
+])
+#X = np.random.randn(6,4)
+#Y = np.random.randn(6,1)
+X = X.T
+Y = Y.T
+Xt = np.array([
+    [0.11,0.41,0.43,0.23],#ans=0
+    [0.12,0.9,0.8,0.99],#ans=0
+    [0.54,0.55,0.41,0.39]#ans=1
+]).T
+print(Y)
+print(X)
+print(Xt)
+model = NeuralNetwork()
+model.fit(X,Y)
+pickle.dump(model,open("model.pkl", "wb"))
+#data = np.array([[1.3,2.4,1.6,1.12]]).T
+model = pickle.load(open("model.pkl", "rb"))
+a2 = model.prediction(Xt)
+print(a2)
+#a2 = model.NewRoadSpeed(float(a2[0]))
+"""
 
